fafff6ecb49f3bfef0b5cd83924ff956bf9ab999
elma_uu <ansvia@naver.com>
2026-05-20 20:04:42 +0900

    Network 클래스 정의

--- game/Network.cpp
+++ game/Network.cpp
@@ -3,5 +3,39 @@
 Network::Network()
 {
 	mSocket = INVALID_SOCKET;
+	ZeroMemory(&mServerAddr, sizeof(mServerAddr));
+}
 
+Network::~Network()
+{
+	if (mSocket != INVALID_SOCKET)
+	{
+		closesocket(mSocket);
+	}
+}
+
+bool Network::Connect(const char* ip, int port)
+{
+	mServerAddr.sin_port = htons(port);
+	mServerAddr.sin_addr.s_addr = inet_addr(ip);
+	mSocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
+	if (mSocket == INVALID_SOCKET)
+	{
+		return false;
+	}
+	int result = WSAConnect(mSocket, (SOCKADDR*)&mServerAddr, sizeof(mServerAddr), NULL, NULL, NULL, NULL);
+	return result != SOCKET_ERROR;
+}
+
+void Network::Send(void* packet)
+{
+	unsigned char* p = reinterpret_cast<unsigned char*>(packet);
+	LPDWORD sent{ 0 };
+	WSASend(mSocket, reinterpret_cast<WSABUF*>(&p), 1, sent, 0, NULL, NULL);
+}
+
+int Network::Receive(char* buffer, int bufferSize)
+{
+	LPDWORD received{ 0 };
+	WSARecv(mSocket, reinterpret_cast<WSABUF*>(&buffer), 1, received, NULL, NULL, NULL);
 }
--- game/Network.h
+++ game/Network.h
@@ -1,17 +1,20 @@
 #pragma once
-#include "winsock.h"
+#include "Windows.h"
+#include "WinSock2.h"
+#include "protocol_2026.h"
 
 class Network
 {
 private:
 	SOCKET mSocket;
 	SOCKADDR_IN mServerAddr;
+	char mBuf[MAX_BUF_SIZE];
 
 public:
 	Network();
 	~Network();
 	bool Connect(const char* ip, int port);
-	void Send(const char* data, int length);
+	void Send(void* packet);
 	int Receive(char* buffer, int bufferSize);
 };
 
