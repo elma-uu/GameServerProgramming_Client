e253ecff0e3752d51b74d102aacb459ca2e11e53
地球が動き出した <79787986+moveofearth@users.noreply.github.com>
2026-05-20 16:04:47 +0900

    Player 기본 정의

---
+++ game/Player.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Object.h"
+
+class Player : public Object
+{
+public:
+	Player();
+	~Player();
+	virtual void Update() override;
+	virtual void LateUpdate() override;
+	virtual void Render(HDC hdc) override;
+
+
+};
+
